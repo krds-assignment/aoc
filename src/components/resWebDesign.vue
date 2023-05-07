5d976381c8ce81954dc5f5f7963f3d07097079c6
Murali <murali@getryt.io>
2023-05-07 19:51:38 +0530

    initial commit

---
+++ src/components/resWebDesign.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="parentElement">
+      <div v-for="detail of templateJsonData" :key="detail.title"
+      class="webLoopElement" :style="{'background-color': detail.bgColor}">
+          <div class="cardHeadElement">
+              <div class="detailsContent">
+                  <div>
+                      <img :src="detail.logo" :alt="detail.title">
+                  </div>
+                  <div>
+                  <p class="title">
+                      {{ detail.title }}
+                  </p>
+                  <p>
+                      {{ detail.desc }}
+                  </p>
+              </div>
+              </div>
+              <div class="desktop">
+                  <img :src="detail.image" :alt="detail.title">
+              </div>
+          </div>
+          <div></div>
+      </div>
+      
+    </div>
+  </template>
+  
+  <script>
+  import { loopData } from '@/js/data.js'
+  export default {
+      data() {
+          return {
+              templateJsonData: [],
+          }
+      },
+      created() {
+          this.templateJsonData = loopData;
+      }
+  }
+  
+  </script>
